41ddc4b45e7852d5fb7ca3afe87622d71c9f38b9
Tim Leisio <timothyleisio@gmail.com>
2016-07-18 23:36:05 -0500

    refactor to simple architecture cause site keeps going down :(

--- routes/routesPublic.js
+++ routes/routesPublic.js
@@ -4,7 +4,7 @@ var artys = require('../app/services/serviceArtys');
 //Public route handling
 module.exports = function(router) {
 
-  router.get('/', function(req,res) {
+  /*router.get('/', function(req,res) {
     //Call a service to return data in HTML format of the markdown artys
     artys.getArtyList({}, function(response) {
       if (response.status === 'success') {
@@ -18,9 +18,51 @@ module.exports = function(router) {
         //TODO: create a logger to capture how often this happens
       }
     });
+  });*/
+
+  router.get('/', function(req,res) {
+    res.render('index', {title:'Articles by Tim Leisio'}, function(err, html) {
+      if (err) { throw(err); }
+      res.send(html);
+    });
   });
 
-  router.get('/artys/:arty', function(req,res,next) {
+  router.get('/artys/product-scope-scenarios-not-features', function(req,res) {
+    res.render('artys/product-scope-scenarios-not-features', {title:'Product Scope by Tim Leisio'}, function(err, html) {
+      if (err) { throw(err); }
+      res.send(html);
+    });
+  });
+
+  router.get('/artys/subtle-insinuation', function(req,res) {
+    res.render('artys/subtle-insinuation', {title:'Subtle Insinuation by Tim Leisio'}, function(err, html) {
+      if (err) { throw(err); }
+      res.send(html);
+    });
+  });
+
+  router.get('/artys/advocating-redesign', function(req,res) {
+    res.render('artys/advocating-redesign', {title:'Advocating Redesign by Tim Leisio'}, function(err, html) {
+      if (err) { throw(err); }
+      res.send(html);
+    });
+  });
+
+  router.get('/scribbles', function(req,res) {
+    res.render('scribbles', {title:'Scribbles by Tim Leisio'}, function(err, html) {
+      if (err) { throw(err); }
+      res.send(html);
+    });
+  });
+
+  router.get('/edp', function(req,res) {
+    res.render('edp', {title:'eDiscovery Point'}, function(err, html) {
+      if (err) { throw(err); }
+      res.send(html);
+    });
+  });
+
+  /*router.get('/artys/:arty', function(req,res,next) {
     var data = {};
     data.url = req.params.arty;
 
@@ -36,7 +78,6 @@ module.exports = function(router) {
         next();
       }
     });
-
-  });
+  });*/
 
 }

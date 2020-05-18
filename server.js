33bd85dbbd35cafb5d9e7c7c41c27d6dbf4c4927
Tim Leisio <timleisio@Tims-MBP-2.zyxel.com>
2020-05-17 22:05:33 -0400

    May updates to include three articles for may and updated about section

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
 var server;
 
 app.use(express.static('public'));
